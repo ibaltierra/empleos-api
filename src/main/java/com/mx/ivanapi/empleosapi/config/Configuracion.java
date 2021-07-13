0ecf930775ff5f84048d509d2ab0ab650ebf4dbc
Ivan Manuel Baltierra Muñoz <mrnar-86@hotmail.com>
2021-07-13 13:33:53 -0500

    Algo

--- src/main/java/com/mx/ivanapi/empleosapi/config/Configuracion.java
+++ src/main/java/com/mx/ivanapi/empleosapi/config/Configuracion.java
@@ -4,8 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
-
-
 @Configuration
 public class Configuracion {
 
